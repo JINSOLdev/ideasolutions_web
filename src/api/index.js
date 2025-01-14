2bd9694fa5e4b65fca61a96a35511c4eface4293
JINSOLdev <130561876+JINSOLdev@users.noreply.github.com>
2025-01-14 14:50:47 +0900

    Update index.js
    기본 axios 인스턴스를 global로 등록

--- src/api/index.js
+++ src/api/index.js
@@ -6,11 +6,11 @@ export const DEV_BASE_URL = 'http://192.168.0.183:8085'; // Mi
 export const API_URL = REMOTE_BASE_URL;
 
 // 기본 axios 인스턴스 설정
-// const defaultAxiosInstance = axios.create({
-//     baseURL: REMOTE_BASE_URL, // 기본 API URL 설정
-//     timeout: 5000,
-//     headers: { 'Content-Type': 'application/json' },
-// });
+const defaultAxiosInstance = axios.create({
+     baseURL: REMOTE_BASE_URL, // 기본 API URL 설정
+     timeout: 5000,
+     headers: { 'Content-Type': 'application/json' },
+});
 
 // 별도 API 인스턴스 생성 함수
 function createApiInstance(baseURL, options = {}) {
@@ -27,4 +27,11 @@ export const posts = createApiInstance(`${API_URL}`);
 export const post = createApiInstance(`${API_URL}`);
 
 // 기본 axios 인스턴스 export
-// export default defaultAxiosInstance;
+export default {
+    install(app) {
+        // 기본 axios 인스턴스를 global로 등록
+        app.config.globalProperties.$axios = defaultAxiosInstance;
+
+        // 필요 시, 다른 API 인스턴스도 등록 가능
+    }
+}
